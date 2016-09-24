5797bb2901e0cd88918266a911557b7236ca6d75
PLaGInc <vladimir.alt@inbox.ru>
2016-09-24 09:46:45 +0300

    First Lab and Selenium test cases
    :8ball: :8ball: :8ball:

---
+++ Rip1.0/src/main/java/com/plag_in/servlet/Cars.java
@@ -0,0 +1,56 @@
+package com.plag_in.servlet;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+/**
+ * Created by Владимир on 18.09.2016.
+ */
+
+
+public class Cars {
+
+    private String mMark;
+    private String mModel;
+    private String mColor;
+    private Integer mYearOfCreate;
+
+    public Cars(String mark, String model, String color, Integer yearOfCreate) {
+        mMark = mark;
+        mModel = model;
+        mColor = color;
+        mYearOfCreate = yearOfCreate;
+    }
+
+    public String getMark() {
+        return mMark;
+    }
+
+    public void setMark(String mark) {
+        this.mMark = mark;
+    }
+
+    public String getModel() {
+        return mModel;
+    }
+
+    public void setModel(String model) {
+        this.mModel = model;
+    }
+
+    public String getColor() {
+        return mColor;
+    }
+
+    public void setColor(String color) {
+        this.mColor = color;
+    }
+
+    public Integer getYearOfCreate() {
+        return mYearOfCreate;
+    }
+
+    public void setYearOfCreate(Integer dateOfCreate) {
+        mYearOfCreate = dateOfCreate;
+    }
+}
---
+++ Rip1.0/src/main/java/com/plag_in/servlet/MainServlet.java
@@ -0,0 +1,138 @@
+package com.plag_in.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Map;
+
+public class MainServlet extends HttpServlet {
+    private final static String sDataKeepName = "dataKeep";
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        ArrayList<Cars> carList = new ArrayList<Cars>();
+        getServletContext().setAttribute(sDataKeepName, carList);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        ServletContext context = req.getServletContext();
+        Map<String, String[]> params = req.getParameterMap();
+        if (params.isEmpty()) {
+            showAddingForm(writer);
+        } else if (params.get("method")[0].equals("showAllCars")) {
+            printAllCars(context, writer);
+        } else if (params.get("method")[0].equals("showAddingForm")) {
+            showAddingForm(writer);
+        } else {
+            super.doGet(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        ServletContext context = req.getServletContext();
+        Map<String, String[]> params = req.getParameterMap();
+        if (params.get("method")[0].equals("addCar")) {
+            addCar(
+                    context,
+                    params.get("mark")[0],
+                    params.get("model")[0],
+                    params.get("color")[0],
+                    params.get("yearOfCreate")[0]);
+
+            showAddingForm(writer);
+        } else {
+            super.doPost(req, resp);
+        }
+    }
+
+    private void showAddingForm(PrintWriter writer) throws IOException {
+        writer.print("<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<body>\n");
+        writer.println("<form action=\"showroom\" method=\"POST\">");
+        writer.print("Car mark:<br>" +
+                        "<input type=\"text\" name=\"mark\"><br>");
+        writer.print("Car model:<br>" +
+                        "<input type=\"text\" name=\"model\"><br>");
+        writer.print("Color:<br>" +
+                        "<input type=\"text\" name=\"color\"><br>");
+        writer.print("Year of create: <br>" +
+                        "<input type=\"year\" name=\"yearOfCreate\">\n" +
+                        "<br>");
+        writer.print("<input type=\"hidden\" name=\"method\" value=\"addCar\">\n" +
+                        "<input type=\"submit\" value=\"Submit\">\n" +
+                        "</form><br>");
+        writer.println("<form action=\"showroom\" method=\"GET\">");
+        writer.print("<input type=\"hidden\" name=\"method\" value=\"showAllCars\">\n" +
+                        "<input type=\"submit\" value=\"Show all cars\">\n");
+        writer.println("</form>\n" +
+                        "</body>\n" +
+                        "</html>");
+    }
+
+    private void printAllCars(ServletContext context, PrintWriter writer) throws IOException {
+        @SuppressWarnings("unchecked")
+        ArrayList<Cars> carList = (ArrayList<Cars>) context.getAttribute(sDataKeepName);
+        String carsOutput = "";
+        for (Cars car : carList) {
+            carsOutput +=
+                    "Mark : " + car.getMark() +
+                            "<br>    Model : " + car.getModel() +
+                            "<br>    Color : " + car.getColor() +
+                            "<br>    Year of create : " + car.getYearOfCreate() +
+                            "<br>";
+        }
+        if (carsOutput.length() == 0) {
+            carsOutput = "<h1>No orders for now!</h1>";
+        }
+
+        writer.print(
+                "<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<body>\n");
+        writer.println(carsOutput);
+        writer.println("<form action=\"showroom\" method=\"GET\">\n" +
+                        "<input type=\"hidden\" name=\"method\" value=\"showAddingForm\">\n" +
+                        "<input type=\"submit\" value=\"Show adding form\">\n" +
+                        "</body>\n" +
+                        "</html>");
+    }
+
+    private void addCar(ServletContext context, String mark, String model,  String color, String yearOfCreate) {
+        Calendar calendar = Calendar.getInstance();
+        int yearToday = calendar.get(Calendar.YEAR);
+        int year;
+        try {
+            year = Integer.parseInt(yearOfCreate);
+        } catch (NumberFormatException ex) {
+            year = yearToday;
+        }
+        //1768 - год создания первого автомобиля
+        if (model.length() > 0 && model.length() <= 50
+                && color.length() > 0 && color.length() <= 50
+                && mark.length() > 0 && mark.length() <= 20
+                && year >= 1768 && year <= yearToday){
+            @SuppressWarnings("unchecked")
+            ArrayList<Cars> carList = (ArrayList<Cars>) context.getAttribute(sDataKeepName);
+            carList.add(new Cars(mark, model, color, year));
+            context.setAttribute(sDataKeepName, carList);
+        }
+    }
+
+}
